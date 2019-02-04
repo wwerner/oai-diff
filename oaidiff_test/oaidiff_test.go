15421c6ea6351e30e8c02b209df64b98cfbbcf34
Wolfgang Werner <w.werner@vhmhv.de>
2019-02-04 17:50:54 +0100

    Play w/ go-cmp

--- oaidiff_test/oaidiff_test.go
+++ oaidiff_test/oaidiff_test.go
@@ -1,15 +1,30 @@
 package oaidiff_test
 
 import (
+	"encoding/json"
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestAddModelProp(t *testing.T) {
 	swaggerLhs, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile("resources/oai-basic.yml")
-	swaggerRhs, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile("resources/oai-basic_add-model.yml")
+	swaggerRhs, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile("resources/oai-basic_add-model-prop.yml")
 
 	require.NoError(t, err)
-	require.Equal(t, swaggerLhs, swaggerRhs)
+
+	opts := []cmp.Option{
+		cmpopts.IgnoreUnexported(
+			openapi3.Schema{},
+		),
+		cmp.Transformer("RawJSONFilter", func(rm json.RawMessage) string {
+			return ""
+		}),
+
+	}
+	if diff := cmp.Diff(swaggerLhs, swaggerRhs, opts...); diff != "" {
+		t.Errorf("%s", diff)
+	}
 }
